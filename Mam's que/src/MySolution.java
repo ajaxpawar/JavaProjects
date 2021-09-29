6c74387ecb52503d684077b11e9b270b66198adc
ajaxpawar <90200831+ajaxpawar@users.noreply.github.com>
2021-09-29 22:05:00 +0530

    Add files via upload

--- Mam's que/src/MySolution.java
+++ Mam's que/src/MySolution.java
@@ -4,26 +4,51 @@ public class MySolution {
 	public Character[] blocksOrder=new Character[numberOfBlocks];
 	public MySolution() {
 		setBlockOrder();
-		printOrder(blocksOrder);
+		if(checkOrder(blocksOrder)) {
+			System.out.println("Blocks are akready in order  !");
+		}
+		else {
+			Character[] tempPosition=new Character[numberOfBlocks];
+			for(int i=0;i<(blocksOrder.length);i++) {
+				System.out.println(blocksOrder[i]+"  move on the table");
+				tempPosition[i]=blocksOrder[i];
+				blocksOrder[i]=' ';
+			}
+			//System.out.println(blocksOrder[(blocksOrder.length)-1]+"  is on the table");
+			//tempPosition[numberOfBlocks-1]=blocksOrder[numberOfBlocks-1];
+			//blocksOrder[numberOfBlocks-1]=' ';
+
+			char temp=(char)(65+numberOfBlocks-1);
+		/*	System.out.println("blockorde:");
+			printOrder(blocksOrder);
+			System.out.println("TempOrde: :");
+			printOrder(tempPosition);*/
+			//System.out.println("temp="+temp);
+			//blocksOrder[(blocksOrder.length)-1]=' ';
+			for (int i=numberOfBlocks-1;i>=0;i--) {
+					
+					for(int j=0;j<tempPosition.length;j++) {
+						if(tempPosition[j]==temp) {
+							blocksOrder[i]=tempPosition[j];
+							
+							tempPosition[j]=' ';
+						}
+					}
+				
+				temp--;
+				printOrder(blocksOrder);
+			}
+		}	
+	}
+	boolean checkOrder(Character[] blocksOrder2) {
 		char temp='A';
-		char temp2;
 		for(int i=0;i<blocksOrder.length;i++) {
 			if(blocksOrder[i]!=temp) {
-				for(int j=i+1;j<blocksOrder.length;j++) {
-					if(blocksOrder[j]==temp) {
-						temp2=blocksOrder[i];
-						blocksOrder[i]=blocksOrder[j];
-						blocksOrder[j]=temp2;
-					}
-				}
+				return false;
 			}
 			temp++;
-			System.out.println("+++++++++++++ ");
-			printOrder(blocksOrder);
-			System.out.println("+++++++++++++ ");
-
 		}
-		
+		return true;
 	}
 	static int getNumberOfBlocks() {
 	    Scanner scan=new Scanner(System.in);
@@ -39,9 +64,15 @@ public class MySolution {
 	}
 	public void printOrder(Character[] list) {
 		for(int i=0;i<list.length;i++) {
-			System.out.println(list[i]);
-			System.out.println("|");
+			if(i==list.length-1) {
+				System.out.println(list[i]+" move on the table");
+			}
+			if(list[i]!=' ' && i!=list.length-1) {
+				System.out.println(list[i]+" move on the "+list[i+1]);
+			}
+			//System.out.println("|");
 		}
+		System.out.println("++++++++++TABLE+++++++++++");
 	}
 	public static void main(String[] args) {
 		numberOfBlocks=getNumberOfBlocks();
